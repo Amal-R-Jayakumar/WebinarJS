9987949153b46f98ecb571eb78f5c8cf4408c22e
Pierre Cavin <services@sherlockstd.io>
2016-11-03 01:12:18 +0100

    Configure auto indent

--- models/Live.js
+++ models/Live.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 
 var LiveSchema = new mongoose.Schema({
-  name: String,
-  youtube: String,
-  date: String,
-  displayDate: String,
-  atelier: { type: String, default: "http://lescuresdedith.com/produit/sejour-retour-a-soi/" },
-  profilage: { type: String, default: "http://lescuresdedith.com/produit/audit-alimentaire/" },
+	name: String,
+	youtube: String,
+	date: String,
+	displayDate: String,
+	atelier: { type: String, default: "http://lescuresdedith.com/produit/sejour-retour-a-soi/" },
+	profilage: { type: String, default: "http://lescuresdedith.com/produit/audit-alimentaire/" },
 });
 
 module.exports = mongoose.model('Live', LiveSchema);
