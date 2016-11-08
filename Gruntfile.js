35ad3b680aa6640db1ce5e7868d5a6cacb6f4af0
Pierre CAVIN <services@sherlockstd.io>
2016-11-08 11:03:23 +0100

    TG-18 #done
    Grunt - Copie des lib users depuis node_modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,37 @@
-module.exports = function(grunt) {
+	module.exports = function(grunt) {
 
-	grunt.initConfig({
-		jshint: {
-			all: ['Gruntfile.js', 'app.js', 'public/js/*/*.js', '!public/js/lib/**', 'routes/**/*.js', 'models/*.js']
-		}
-	});
+		grunt.initConfig({
+			jshint: {
+				all: ['Gruntfile.js', 'app.js', 'public/js/*/*.js', '!public/js/lib/**', 'routes/**/*.js', 'models/*.js']
+			},
+			copy: {
+				single: {
+			files: [
+					{src: ['node_modules/angular/angular.min.js'], dest: 'public/js/lib/angular.min.js'},
+					{src: ['node_modules/angular/angular.min.js.map'], dest: 'public/js/lib/angular.min.js.map'},
+					{src: ['node_modules/angular-resource/angular-resource.min.js'], dest: 'public/js/lib/angular-resource.min.js'},
+					{src: ['node_modules/angular-resource/angular-resource.min.js.map'], dest: 'public/js/lib/angular-resource.min.js.map'},
+					{src: ['node_modules/angular-route/angular-route.min.js'], dest: 'public/js/lib/angular-route.min.js'},
+					{src: ['node_modules/angular-route/angular-route.min.js.map'], dest: 'public/js/lib/angular-route.min.js.map'},
+					{src: ['node_modules/angular-socket-io/socket.min.js'], dest: 'public/js/lib/socket.min.js'},
+					{src: ['node_modules/angular-socket-io/socket.min.js.map'], dest: 'public/js/lib/socket.min.js.map'},
+					{src: ['node_modules/angular-moment/angular-moment.min.js'], dest: 'public/js/lib/angular-moment.min.js'},
+					{src: ['node_modules/angular-moment/angular-moment.min.js.map'], dest: 'public/js/lib/angular-moment.min.js.map'},
+					{src: ['node_modules/moment/min/moment.min.js'], dest: 'public/js/lib/moment.min.js'},
+					{src: ['node_modules/moment-locale-fr/fr.js'], dest: 'public/js/lib/moment.fr.js'}
+				]
+				}
+			},
+			clean: [
+				'public/js/lib'
+			]
+		});
 
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+		grunt.loadNpmTasks('grunt-contrib-jshint');
+		grunt.loadNpmTasks('grunt-contrib-copy');
+		grunt.loadNpmTasks('grunt-contrib-clean');
 
-	grunt.registerTask('default', ['jshint']);
-	grunt.registerTask('test', ['jshint']);
-};
+		grunt.registerTask('default', ['jshint']);
+		grunt.registerTask('test', ['jshint']);
+		grunt.registerTask('build', ['jshint', 'clean', 'copy']);
+	};
