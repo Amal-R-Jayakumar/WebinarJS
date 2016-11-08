ef21978b14e84240e488e052b8aad6e07b145872
Pierre CAVIN <services@sherlockstd.io>
2016-11-08 23:00:53 +0100

    TG-22 #done & TG-23 #done Implementer socket.io & Utiliser socket.io pour transferer les messages

--- client/js/live/controllers.js
+++ client/js/live/controllers.js
@@ -1,20 +1,62 @@
 app.controller('LiveDisplayCtrl', ['$scope', '$routeParams', 'LiveFactory', 'ChatFactory', 'socket', '$location', function ($scope, $routeParams, LiveFactory, ChatFactory, socket, $location, $sce) {
-  $scope.live = LiveFactory.get({ id: $routeParams.id });
+  $scope.live = LiveFactory.get({
+    id: $routeParams.id
+  });
 
   $scope.messages = {};
 
-  $scope.messages = ChatFactory.query({ thread: $routeParams.id });
 
-  $scope.save = function(){
-    if(!$scope.newMessage.content) return;
-    var message = new ChatFactory({ user: $scope.newMessage.user, content: $scope.newMessage.content, thread: $routeParams.id });
+  $scope.messages = ChatFactory.query({
+    thread: $routeParams.id
+  });
 
-    message.$save(function(){
+  /* $scope.save = function () {
+    if (!$scope.newMessage.content) return;
+    var message = new ChatFactory({
+      user: $scope.newMessage.user,
+      content: $scope.newMessage.content,
+      thread: $routeParams.id
+    });
+
+    message.$save(function () {
       $scope.messages.push(message);
       $scope.newMessage = ''; // clear textbox
     });
-  };
-  socket.on('messages', function (messages, $scope) {
-    console.log(messages);
+  }; */
+
+  //Socket listeners
+
+  socket.on('init', function (data) {
+    console.log(data);
   });
+
+  socket.emit('get:messages', $routeParams.id);
+
+  socket.on('get:messages', function (messages) {
+    $scope.messages = messages;
+  });
+
+  socket.on('send:message', function (message) {
+    $scope.messages.push(message);
+  });
+
+
+  // Methods published to the scope
+  // ==============================
+
+  $scope.sendMessage = function () {
+    socket.emit('send:message', {
+      content: $scope.newMessage.content,
+      thread: $routeParams.id
+    });
+
+    // add the message to our model locally
+    $scope.messages.push({
+      content: $scope.newMessage.content
+    });
+
+    // clear message box
+    $scope.newMessage.content = '';
+  };
+
 }]);
--- routes/socket.js
+++ routes/socket.js
@@ -1,21 +1,41 @@
-// Functions for socket module
-var foo = (function () {
+var socketio = require('socket.io');
+var Chat = require('../models/Chat.js');
 
-  var bar = function () {
-    // function foo.bar();
-  };
+var thread = "581cc1d5a979f36f0fd018f5";
+console.log(thread);
 
-  var baz = function () {
-    // function foo.baz();
-  };
+module.exports.listen = function (app) {
+  io = socketio.listen(app);
 
-}());
+  var connected = 0;
 
+  io.sockets.on('connection', function (socket) {
+    io.sockets.emit('init', 'init data');
 
-// export function for listening to the socket
-module.exports = function (socket) {
-  console.log('a user connected');
-  socket.on('disconnect', function () {
-    console.log('a user disconnected');
+    socket.on('get:messages', function (thread) {
+      Chat.find({
+        'thread': thread
+      }, function (err, messages) {
+        io.sockets.emit('get:messages', messages);
+      });
+    });
+
+    connected++;
+    console.log(connected + " clients connected");
+
+    socket.on('disconnect', function () {
+      connected--;
+      console.log(connected + " clients connected");
+    });
+
+    socket.on('send:message', function (message) {
+      Chat.create(message, function (err, success) {
+        if (err) return next(err);
+        console.log('New message : ' + success);
+      });
+      socket.broadcast.emit('send:message', message);
+    });
   });
+
+  return io;
 };
